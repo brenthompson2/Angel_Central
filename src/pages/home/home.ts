31d4c0f88ffdc24c15c5b7a382cfd212836f8332
David Lee Morrow <d.lee.morrow@gmail.com>
2018-08-15 16:03:56 -0400

    fix audio issues on android and ios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,15 +27,28 @@ export class HomePage {
 
 		platform.ready().then(() => {
             statusBar.styleDefault();
-            if (platform.is('android')) {
+            // native audio does not work on android
+            if(platform.is('android')) {
                 statusBar.overlaysWebView(false);
                 statusBar.backgroundColorByHexString('#000000');
+                var background_music = new Audio("assets/Angel_Central.wav");
+				background_music.play();
+				// background_music.loop = true; removed for consistency due to broken functionality in native audio for ios
+            }
+            else{
+            	console.log("platform ios");
+                nativeAudio.preloadComplex("Angel_Central", "/assets/Angel_Central.wav", 1, 1, 0).then(onSuccess => {
+                    console.log("audio load successful");
+                    this.nativeAudio.play("Angel_Central").then(onSuccess => {
+                        console.log("audio play successful");
+                    }, onError => {
+                        console.log("audio play failure");
+                    });
+                }, onError => {
+                    console.log("audio load failure");
+                });
             }
         });
-
-		var background_music = new Audio("assets/Angel_Central.wav");
-		background_music.play();
-		background_music.loop = true;
 
 		initializeApp(FIREBASE_CONFIG); // Initialize app w/ firebase
 	}
